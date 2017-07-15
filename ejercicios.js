052bb5ebad6538226d873fefe56946098200b196
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-07-14 17:23:05 -0700

    Subiendo nuevos ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,10 @@
+//AGREGAR SUS FUNCIONES
+function Ejercicio1(){
+    var arregloDado = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    var arregloInvertido = [];
+
+    document.write("Arreglo invertido: " + invertirArreglo(arregloDado));
+    arregloInvertido = arregloDado.reverse();
+    return arregloInvertido.join(" ");
+
+}
